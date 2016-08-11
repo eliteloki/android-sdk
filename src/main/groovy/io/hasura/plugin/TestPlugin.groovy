4f3f4688fc6a079b774ceb1d7852bdf6b06814f2
Tanmai Gopal <tanmaig@34cross.in>
2016-08-12 03:14:20 +0530

    Adds a dependent task

--- src/main/groovy/io/hasura/plugin/TestPlugin.groovy
+++ src/main/groovy/io/hasura/plugin/TestPlugin.groovy
@@ -4,6 +4,7 @@ import io.hasura.db.util.Configuration
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import io.hasura.db.util.GenerationUtil
+// import org.gradle.api.tasks.GradleBuild
 
 /**
  * Created by tanmaigopal on 11/08/16.
@@ -11,23 +12,35 @@ import io.hasura.db.util.GenerationUtil
 class TestPlugin implements Plugin<Project> {
     @Override
     void apply(Project target) {
-        def dbCodegenTask = target.tasks.create("dbCodegen") << {
+        Properties properties = new Properties()
+        properties.load(target.rootProject.file('hasura.properties').newDataInputStream())
+        def adminToken = properties.getProperty('admin.token')
 
+        def dbCodegenTask = target.tasks.create('dbcodegen') << {
             println 'Running dbCodegen. So exciting!...'
+
             // Build Generation Configuration
             // ------------------------------
             def cfg = new Configuration();
             cfg.setDir('chumma')
-            cfg.setPackageName('io.hasura-app.project76.data')
-            cfg.setDBUrl('https://data.nonslip53.hasura-app.io')
+            cfg.setPackageName('io.hasura.' + target.projectName + '.data')
+            cfg.setDBUrl('https://data.' + target.projectName + '.hasura-app.io')
             cfg.setDBPrefix('/v1')
-            cfg.setAdminAPIKey('phpez9z174xdsc8gs0b6f1zobi7m0l2p')
+            cfg.setAdminAPIKey(adminToken)
 
             // Run the code generator
             // ----------------------
             GenerationUtil.generate(cfg)
         }
-        dbCodegenTask.group = "DB Codegen"
-        dbCodegenTask.description = "Generates tables by looking at the schema"
+        dbCodegenTask.group = "codegen"
+        dbCodegenTask.description = "Just generates the tables code"
+
+        def codegenTask = target.tasks.create("codegen").dependsOn(dbCodegenTask) {
+            println 'DBCodegen will execute before me. Now that dbCodegen is done, I shall copy/compile them into a JAR'
+            println target.projectName
+        }
+
+        codegenTask.group = "codegen"
+        codegenTask.description = "Overall task that does all the codegen and builds a JAR in the right place"
     }
 }
