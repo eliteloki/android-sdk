5ae2600ac77ac5587b69fac88d8e307940cdd397
lokesh <lokesh@34cross.in>
2016-08-27 04:19:37 +0530

    added for working veersion of Hasura sdk intergration

--- example-hasura-todo-project/app/src/main/java/com/elite/todo_sdk_test/TodoApplication.java
+++ example-hasura-todo-project/app/src/main/java/com/elite/todo_sdk_test/TodoApplication.java
@@ -1,6 +1,8 @@
 package com.elite.todo_sdk_test;
 
 import android.app.Application;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
 import io.hasura.core.Hasura;
@@ -10,10 +12,11 @@ import io.hasura.core.PersistentCookieStore;
  * Created by loki on 18/08/16.
  */
 
-public class TodoApplication extends Application {
+public class TodoApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        MultiDex.install(getApplicationContext());
         Log.i(getClass().getSimpleName(),"Before clear"+String.valueOf(new PersistentCookieStore(getApplicationContext()).getCookies()));
         (new PersistentCookieStore(getApplicationContext())).removeAll();
         Log.i(getClass().getSimpleName(),"After clear"+String.valueOf(new PersistentCookieStore(getApplicationContext()).getCookies()));
